924a2048614ff92a2f994b9ac7aee9af7688c3e2
trinhhcse <trinhhcse@gmail.com>
2017-02-26 08:35:31 +0700

    create Service

--- BookLibraryService/BookLibraryService.svc.cs
+++ BookLibraryService/BookLibraryService.svc.cs
@@ -8,9 +8,9 @@ using System.Text;
 
 namespace BookLibraryService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BookLibraryService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select BookLibraryService.svc or BookLibraryService.svc.cs at the Solution Explorer and start debugging.
+    public class BookLibraryService : IBookLibraryService
     {
         public string GetData(int value)
         {
