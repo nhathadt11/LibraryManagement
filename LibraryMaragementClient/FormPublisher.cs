caa13b5ec6cea1d2a040ac460c77c784f969f5d9
trinhhcse <trinhhcse@gmail.com>
2017-02-26 15:48:25 +0700

    A

--- LibraryMaragementClient/FormPublisher.cs
+++ LibraryMaragementClient/FormPublisher.cs
@@ -1,24 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Service;
+using System;
 using System.Windows.Forms;
 
 namespace LibraryMaragementClient
 {
     public partial class FormPublisher : Form
     {
-        public FormPublisher()
+        private PublisherService _service;
+        private FormPublisher _instance;
+        private FormPublisher()
         {
             InitializeComponent();
+            _service = new PublisherService();
+        }
+        public FormPublisher Instance
+        {
+            get
+            {
+                if (_instance == null || _instance.IsDisposed)
+                {
+                    _instance = new FormPublisher();
+                }
+                return _instance;
+            }
         }
 
         private void FormPublisher_Load(object sender, EventArgs e)
         {
+            dgvPublishers.DataSource = _service.GetAll();
 
         }
     }
