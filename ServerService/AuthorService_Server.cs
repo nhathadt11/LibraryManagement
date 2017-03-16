9f1b7c5f58cf27aba44cf01e159bb3255ef60743
root <ducthoai@outlook.com>
2017-03-17 02:18:46 +0700

    add server and server service (author only, not config)

---
+++ ServerService/AuthorService_Server.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseAccess.DataTransferObjects;
+using Service;
+namespace Server
+{
+    class AuthorService_Server : IAuthorServer
+    {
+        AuthorService _authorService = new AuthorService();
+        public AuthorService_Server()
+        {
+
+        }
+        public int Add(Author author)
+        {
+            return _authorService.Add(author);
+        }
+
+        public int Delete(int authorId)
+        {
+            return _authorService.Delete(authorId);
+        }
+
+        public DataTable GetAll()
+        {
+            return _authorService.GetAll();
+        }
+
+        public int Update(Author author)
+        {
+            return _authorService.Update(author);
+        }
+    }
+}
